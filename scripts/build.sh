86c98e47d780aa4bf140455ced03b2ae405de64e
Felix Imobersteg <felix@whatwedo.ch>
2018-12-22 21:23:03 +0100

    feat(core): adds new build script

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+
+# Preparation
+if [[ $# -ne 2 ]]; then
+    echo 'Usage ./build.sh [image-name] [tag]'
+    exit 1
+fi
+
+# Configuration
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMAGE_NAME=$1
+TAG=$2
+IMAGE_DIR=$DIR/../images/$IMAGE_NAME
+IMAGE_REPO=whatwedo/$IMAGE_NAME
+
+# Build
+docker build -t $IMAGE_REPO:$TAG --build-arg BASE_IMAGE_TAG=$TAG --no-cache $IMAGE_DIR
