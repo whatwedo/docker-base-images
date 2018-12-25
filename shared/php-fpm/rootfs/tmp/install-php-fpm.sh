37eb60d90b3758525d969fdf53fb268ecabee443
Felix Imobersteg <felix@whatwedo.ch>
2018-12-25 23:25:08 +0100

    feat(nginx-php): adds v2.0 image

---
+++ shared/php-fpm/rootfs/tmp/install-php-fpm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+# Configuration
+[ -z "$PHP_MAJOR_VERSION" ] && echo "PHP_MAJOR_VERSION is not set" && exit 1;
+[ -z "$PHP_MINOR_VERSION" ] && echo "PHP_MINOR_VERSION is not set" && exit 1;
+
+# Install PHP-FPM
+apk add --no-cache php-fpm@php
+
+# Configure PHP-FPM
+sed -i s,^listen\ =\ .*,listen\ =\ /var/run/php-fpm.sock,g /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "listen.owner = nginx" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "listen.group = nginx" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "php_admin_value[upload_max_filesize] = 32M" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "php_admin_value[post_max_size] = 32M" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "php_flag[display_errors] = off" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "php_admin_flag[log_errors] = on" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "php_flag[expose_php] = Off" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "clear_env = no" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "catch_workers_output = yes" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
+echo "clear_env = no" >> /etc/php$PHP_MAJOR_VERSION/php-fpm.d/www.conf
--- shared/php/rootfs/tmp/install-php.sh
+++ shared/php/rootfs/tmp/install-php.sh
@@ -4,7 +4,8 @@
 set -e
 
 # Configuration
-PHP_VERSION=7.3
+[ -z "$PHP_MAJOR_VERSION" ] && echo "PHP_MAJOR_VERSION is not set" && exit 1;
+[ -z "$PHP_MINOR_VERSION" ] && echo "PHP_MINOR_VERSION is not set" && exit 1;
 
 # Install build dependencies
 apk add --no-cache --virtual .build-deps curl
@@ -12,7 +13,7 @@ apk add --no-cache --virtual .build-deps curl
 # Add repository
 ALPINE_VERSION=`cat /etc/alpine-release | cut -d'.' -f-2`
 curl https://php.codecasts.rocks/php-alpine.rsa.pub -o /etc/apk/keys/php-alpine.rsa.pub
-echo "@php https://php.codecasts.rocks/v$ALPINE_VERSION/php-$PHP_VERSION" >> /etc/apk/repositories
+echo "@php https://php.codecasts.rocks/v$ALPINE_VERSION/php-$PHP_MINOR_VERSION" >> /etc/apk/repositories
 
 # Remove build dependencies
 apk del --no-cache .build-deps
@@ -30,10 +31,16 @@ apk add --no-cache php@php \
     php-mbstring@php \
     php-pdo_mysql@php \
     php-pdo_sqlite@php \
+    php-phar@php \
     php-soap@php \
     php-xml@php \
     php-zip@php \
     composer
 
+# Configure PHP
+sed -i s/^upload_max_filesize.*/upload_max_filesize\ =\ 50M/g /etc/php$PHP_MAJOR_VERSION/php.ini
+sed -i s/^post_max_size.*/post_max_size\ =\ 50M/g /etc/php$PHP_MAJOR_VERSION/php.ini
+echo "error_log = /dev/stderr" >> /etc/php$PHP_MAJOR_VERSION/php.ini
+
 # Add CLI symlink
-ln -s /usr/bin/php`echo $PHP_VERSION | cut -d'.' -f 1` /usr/bin/php
+ln -s /usr/bin/php$PHP_MAJOR_VERSION /usr/bin/php
