37663cc7caa4d49c489e5d5ba8baf787ea789445
Felix Imobersteg <felix@whatwedo.ch>
2019-02-12 16:01:04 +0100

    fix(core): fixes update-readme.sh script

--- update-readme.sh
+++ update-readme.sh
@@ -4,17 +4,21 @@
 set -e
 
 # Configuration
-[ -z "$DOCKERHUB_USERNAME" ] && echo "Need to set DOCKERHUB_USERNAME" && exit 1;
-[ -z "$DOCKERHUB_PASSWORD" ] && echo "Need to set DOCKERHUB_PASSWORD" && exit 1;
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+[ -z "$DOCKERHUB_USERNAME" ] && echo "Need to set DOCKERHUB_USERNAME" && exit 1
+[ -z "$DOCKERHUB_PASSWORD" ] && echo "Need to set DOCKERHUB_PASSWORD" && exit 1
+DIR="$(dirname "$SCRIPT")"
 
-# Update README's
-for IMAGE_DIR in $(find $DIR/images/* -maxdepth 1 -type f -name README.md -exec dirname {} \;); do
-    IMAGE_NAME=${IMAGE_DIR##*/}
-    if [[ "$IMAGE_NAME" =~ ^(base|symfony2|symfony3|symfony4|nginx)$ ]]; then
-        echo Skipping $IMAGE_NAME
-    else
-        echo Upating README of $IMAGE_NAME
-        docker run --rm -v $IMAGE_DIR/README.md:/data/README.md -e DOCKERHUB_USERNAME=$DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD=$DOCKERHUB_PASSWORD -e DOCKERHUB_REPO_PREFIX=whatwedo -e DOCKERHUB_REPO_NAME=$IMAGE_NAME sheogorath/readme-to-dockerhub
-    fi
+# Update all README's
+# for IMAGE_DIR in $(find $DIR/images/* -maxdepth 1 -type f -name README.md -exec dirname {} \;); do
+#     IMAGE_NAME=${IMAGE_DIR##*/}
+#     echo Upating README of $IMAGE_NAME
+#     # echo docker run --rm -v $IMAGE_DIR/README.md:/data/README.md -e DOCKERHUB_USERNAME=$DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD=$DOCKERHUB_PASSWORD -e DOCKERHUB_REPO_PREFIX=whatwedo -e DOCKERHUB_REPO_NAME=$IMAGE_NAME sheogorath/readme-to-dockerhub
+# done
+
+# Update given README's
+for IMAGE in "nginx-php" "php" "yarn"; do
+    echo Upating README of $IMAGE
+    echo docker run --rm -v `pwd`/images/$IMAGE/README.md:/data/README.md -e DOCKERHUB_USERNAME=$DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD=$DOCKERHUB_PASSWORD -e DOCKERHUB_REPO_PREFIX=whatwedo -e DOCKERHUB_REPO_NAME=$IMAGE sheogorath/readme-to-dockerhub
 done
+
+# if [[ "$IMAGE_NAME" =~ ^(base|symfony2|symfony3|symfony4|nginx)$ ]]; then
