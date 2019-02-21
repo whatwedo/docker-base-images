a93b827c72bbe3417f870e03c9713ea717d0d750
Felix Imobersteg <felix@whatwedo.ch>
2019-02-21 14:25:06 +0100

    feat(v2.0): automatically update all readme's on dockerhub

--- update-readme.sh
+++ update-readme.sh
@@ -6,19 +6,10 @@ set -e
 # Configuration
 [ -z "$DOCKERHUB_USERNAME" ] && echo "Need to set DOCKERHUB_USERNAME" && exit 1
 [ -z "$DOCKERHUB_PASSWORD" ] && echo "Need to set DOCKERHUB_PASSWORD" && exit 1
-DIR="$(dirname "$SCRIPT")"
 
 # Update all README's
-# for IMAGE_DIR in $(find $DIR/images/* -maxdepth 1 -type f -name README.md -exec dirname {} \;); do
-#     IMAGE_NAME=${IMAGE_DIR##*/}
-#     echo Upating README of $IMAGE_NAME
-#     # echo docker run --rm -v $IMAGE_DIR/README.md:/data/README.md -e DOCKERHUB_USERNAME=$DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD=$DOCKERHUB_PASSWORD -e DOCKERHUB_REPO_PREFIX=whatwedo -e DOCKERHUB_REPO_NAME=$IMAGE_NAME sheogorath/readme-to-dockerhub
-# done
-
-# Update given README's
-for IMAGE in "nginx-php" "php" "yarn"; do
-    echo Upating README of $IMAGE
-    docker run --rm -v `pwd`/images/$IMAGE/README.md:/data/README.md -e DOCKERHUB_USERNAME=$DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD=$DOCKERHUB_PASSWORD -e DOCKERHUB_REPO_PREFIX=whatwedo -e DOCKERHUB_REPO_NAME=$IMAGE sheogorath/readme-to-dockerhub
+for IMAGE_DIR in $(find ./images/* -maxdepth 1 -type f -name README.md -exec dirname {} \;); do
+    IMAGE_NAME=${IMAGE_DIR##*/}
+    echo Upating README of $IMAGE_NAME
+    docker run --rm -v `pwd`/images/$IMAGE_NAME/README.md:/data/README.md -e DOCKERHUB_USERNAME=$DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD=$DOCKERHUB_PASSWORD -e DOCKERHUB_REPO_PREFIX=whatwedo -e DOCKERHUB_REPO_NAME=$IMAGE_NAME sheogorath/readme-to-dockerhub
 done
-
-# if [[ "$IMAGE_NAME" =~ ^(base|symfony2|symfony3|symfony4|nginx)$ ]]; then
