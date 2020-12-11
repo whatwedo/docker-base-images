941d05a2172b9186661e527cc5c2d9c8685267f0
Ueli Banholzer <ueli@whatwedo.ch>
2020-12-11 23:46:35 +0100

    fix(ci): fix readme validation
    (cherry picked from commit dae775d36b66114f826dc34a328d19ac17dbdd84)

--- validate-readme.sh
+++ validate-readme.sh
@@ -5,11 +5,11 @@ set -e
 
 # Validating main README
 echo Validating main README
-docker run --rm -i paasmule/markdown-link-check < README.md
+docker run -v ${PWD}:/data:ro --rm -i ghcr.io/tcort/markdown-link-check:stable /data/README.md
 
 # Validating all README's
 for IMAGE_DIR in $(find ./images/* -maxdepth 1 -type f -name README.md -exec dirname {} \;); do
     IMAGE_NAME=${IMAGE_DIR##*/}
     echo Validating README of $IMAGE_NAME
-    docker run --rm -i paasmule/markdown-link-check < images/$IMAGE_NAME/README.md
+    docker run -v ${PWD}:/data:ro --rm -i ghcr.io/tcort/markdown-link-check:stable /data/images/$IMAGE_NAME/README.md
 done
