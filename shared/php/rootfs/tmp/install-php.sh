a49cef4184f81a327b58b147f4ab3ba3a8d443bd
Ueli Banholzer <ueli@whatwedo.ch>
2024-02-04 18:02:02 +0100

    fix(php): use imagick 3.7.0

--- shared/php/rootfs/tmp/install-php.sh
+++ shared/php/rootfs/tmp/install-php.sh
@@ -54,14 +54,16 @@ apk add --no-cache php$PHP_VERSION \
     gcc \
     musl-dev
 
-# install imagick from PR
-#     https://github.com/Imagick/imagick/pull/641
+
+
+# Imagick is installed from the archive because regular installation fails
+# See: https://github.com/Imagick/imagick/issues/643#issuecomment-1834361716
 # replace the routine with this command after PR has been merged:
 #     pecl$PHP_VERSION install imagick 
 cd /tmp
-wget -O imagick.tar.gz https://github.com/FedericoHeichou/imagick/archive/7088edc353f53c4bc644573a79cdcd67a726ae16.tar.gz
+wget -O imagick.tar.gz https://github.com/Imagick/imagick/archive/refs/tags/3.7.0.tar.gz
 tar xvfz imagick.tar.gz
-cd imagick-7088edc353f53c4bc644573a79cdcd67a726ae16
+cd imagick-3.7.0
 phpize$PHP_VERSION
 ./configure --with-php-config=/usr/bin/php-config$PHP_VERSION --with-imagick
 make
